0dfa7a287ca98f2a6df7319ab5d0c478db8c21b1
Daniel Rodriguez <124081286+rdaniis@users.noreply.github.com>
2023-04-14 11:07:27 -0600

    Video final

--- src/app/videopopup/videopopup.page.ts
+++ src/app/videopopup/videopopup.page.ts
@@ -5,12 +5,18 @@ import {SafeResourceUrl, DomSanitizer} from '@angular/platform-browser'
   selector: 'app-videopopup',
   templateUrl: './videopopup.page.html',
   styleUrls: ['./videopopup.page.scss'],
+  
 })
 export class VideopopupPage implements OnInit  {
-//@Input()
-//urlSafe: SafeResourceUrl;
+oniput(){
+
+
+  
+}
+urlSafe: any[] = [];
+SafeResourceUrl:any[]=[];
   constructor(private modalController: ModalController, public sanitizer: DomSanitizer ) { }
-  //passurl: string;
+  passurl: any = [""];
 ngOnInit() {
   //this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.passurl);
   console.log('${this.urlSafe}');
