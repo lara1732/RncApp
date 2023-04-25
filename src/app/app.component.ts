0bd5520adabde39dce08a27e675f629c8f696722
lara1732 <119022714+lara1732@users.noreply.github.com>
2023-04-25 10:54:50 -0600

    initalizeApp 1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -149,7 +149,112 @@ export class AppComponent {
       });// Fin Pause
       this.platform.resume.subscribe(async () => {
 
-        alert("resume");
+        await this.storage.create();
+
+     /* this.toast.show('First Time', '10000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );*/
+
+      let user =  await this.storage.get('login');
+      let pass =  await this.storage.get('pass');
+
+      if(user != null && pass != null){
+
+        $("#Preloader").show();
+        $("#ButtonLogin").attr('disabled','disabled');
+        $("#ButtonResetLogin").attr('disabled','disabled');
+
+        $.ajax({
+          url: this.URL_Link+'login/singin',
+          type:'POST',
+          dataType: "text",
+          data:{login:user, pass:pass, type:"m"},
+          crossDomain: true,
+          async: true,
+          success:(data) =>{
+
+            var obj = JSON.parse(data);
+
+            if (obj != "") {
+
+              var msg = obj;
+
+              if (msg == 'OK-') {
+
+                $.ajax({
+                  url: ('https://backup.tregional.mx/AbetCloud/models/queries/app/identify.php'),
+                  type:'POST',
+                  dataType: "text",
+                  data:{login:user, pass:pass},
+                  crossDomain: true,
+                  async: false,
+                  success:(dataId) =>{
+                  
+                    this.storage.set("id",dataId);
+                    this.permisos(dataId);    
+                                 
+                    
+                  }
+                })
+
+                $("#Preloader").hide();
+                $("#ButtonLogin").removeAttr('disabled');
+                $("#ButtonResetLogin").removeAttr('disabled');
+
+
+                this.router.navigate(['/home']);
+         
+              }else if(obj == "IUOP"){
+                $("#Preloader").hide();
+                $("#ButtonLogin").removeAttr('disabled');
+                $("#ButtonResetLogin").removeAttr('disabled');
+                Swal.fire({title:'Error', icon:'error', text: 'User or incorrect password',heightAuto:false});
+              }else if (obj == "UWOA") {
+                $("#Preloader").hide();
+                $("#ButtonLogin").removeAttr('disabled');
+                $("#ButtonResetLogin").removeAttr('disabled');
+                Swal.fire({title:'Error', icon:'error', text: 'User without access to this app',heightAuto:false});
+              }else if (obj == "UWAS") {
+
+                this.router.navigate(['/home']);
+    
+              }
+            }
+          },error:function(status, textStatus, jqXHR){
+
+            if (status.statusText=="timeout") {
+
+              Swal.fire({   
+                title: 'Error',
+                text: 'Your device is not connected to internet or your connection is very slow.\n Please try again' ,   
+                icon: 'error',   
+                heightAuto:false,
+                allowOutsideClick: false,
+                showCancelButton: false,   
+                confirmButtonColor: "#DD6B55",   
+                confirmButtonText: "OK",   
+                cancelButtonText: "No, Cancelar",   
+              }).then((result) => {
+            if (result.value) {
+                $("#Preloader").hide();
+                $("#ButtonLogin").removeAttr('disabled');
+                $("#ButtonResetLogin").removeAttr('disabled');
+
+                  } 
+              });
+            }else{
+              $("#Preloader").hide();
+              $("#ButtonLogin").removeAttr('disabled');
+              $("#ButtonResetLogin").removeAttr('disabled');
+              Swal.fire({title:'Error', icon:'error', text: 'An internal server error has occurred please contact the site admin',heightAuto:false});
+            }
+    
+          }
+        });  
+
+      }
 
 
         
