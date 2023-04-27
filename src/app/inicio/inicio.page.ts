e69d92b7af6bef0d795a7f011d958206b5a8c09d
Dexx6 <ivanromeropga@gmail.com>
2023-04-27 10:36:05 -0600

    Fecha console

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,15 +1,18 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { Router } from '@angular/router';
 import * as $ from "jquery";
 import Swal from 'sweetalert2';
-import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi, Calendar } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import { FullCalendarComponent } from '@fullcalendar/angular';
 import esLocale from '@fullcalendar/core/locales/es';
 import { async } from '@angular/core/testing';
+import * as moment from 'moment';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -18,6 +21,8 @@ import { async } from '@angular/core/testing';
 })
 
 export class InicioPage implements OnInit {
+
+  @ViewChild('Calendar') calendarComponent: FullCalendarComponent;
 
   link="";
   calendarVisible = true;
@@ -44,7 +49,7 @@ export class InicioPage implements OnInit {
     
     headerToolbar: {
       left: 'prev,next today',
-      center: 'title',
+      center: '',
       right: 'dayGridMonth,timeGridWeek,listWeek'
     },
     
@@ -149,6 +154,9 @@ export class InicioPage implements OnInit {
     this.link = await this.storage.get('link');  
     console.log(this.link);
     this.calendarOptions.events=this.link
+    let calendarApi = this.calendarComponent.getApi();
+    var date = calendarApi.getDate();
+    console.log(moment(date).format('YYYY-MM-DD'));
 
   }
   
